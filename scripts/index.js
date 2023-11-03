d29d1097d64c9b6c7c123c7fa2a8a511d1b0b533
JMura18 <jmurakam@macalester.edu>
2023-11-03 13:28:10 -0500

    index.js
    cleaned up code

--- scripts/index.js
+++ scripts/index.js
@@ -64,18 +64,6 @@ signInWithPopup(auth, provider)
     const credential = GoogleAuthProvider.credentialFromError(error);
     // ...
   });
-
-//function firebaseApp() {
-  //return (
-  //<div index="firebaseApp">
-       //<button class="login-with-google-btn" onClick={signInWithGoogle}>
-       //</button>
-       //<h1>{localStorage.getItem("name")}</h1>
-       //<h1>{localStorage.getItem("email")}</h1>
-       //</div>
-    //);
-  //n}
-
 // ------------------------------------------------------------ Run app 
 
 // check if a user is currently logged in

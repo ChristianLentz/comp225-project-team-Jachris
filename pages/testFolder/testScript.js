d4a3690367e5703cd4d1d21e0ad8eb9cd059571e
jacobhellenbrand <112660239+jacobhellenbrand@users.noreply.github.com>
2023-11-06 15:41:59 -0600

    some stuff with enlarging the cards on home page, some bugs with multiple cards in columns need to place right most cards left

--- pages/testFolder/testScript.js
+++ pages/testFolder/testScript.js
@@ -38,27 +38,77 @@
 //       card.className += " showBack";
 //   }
 // }
+// document.addEventListener("DOMContentLoaded", function () {
+//   const postGrid = document.querySelector(".postGrid");
+//   const postArea = document.querySelector(".postArea");
+
+//   if (postGrid && postArea) {
+//     postGrid.addEventListener("click", function (e) {
+//       const clickedCard = e.target.closest(".flipdiv.v");
+//       if (clickedCard) {
+//         if (postArea.classList.contains("enlarged")) {
+//           postArea.classList.remove("enlarged");
+//           clickedCard.style.transform = "scale(1)";
+//          } else {
+         
+//         //   postArea.classList.add("enlarged");
+//         //   clickedCard.style.transform = "scale(1.2)";
+//         //   // const translateX = `calc(-${column} * (100% + 20px))`;
+//         //   // clickedCard.style.transform = `scale(1.2) translateX(${translateX})`;
+//         //   bringCardToFront(clickedCard);
+//         }
+//         toggleFlip(clickedCard);
+//       }
+//     });
+//   }
+// });
+
+
 document.addEventListener("DOMContentLoaded", function () {
   const postGrid = document.querySelector(".postGrid");
   const postArea = document.querySelector(".postArea");
+  let enlargedCard = null;
 
   if (postGrid && postArea) {
     postGrid.addEventListener("click", function (e) {
       const clickedCard = e.target.closest(".flipdiv.v");
+      if(!clickedCard) {
+        enlargedCard.style.zIndex = "0";
+      }
       if (clickedCard) {
-        if (postArea.classList.contains("enlarged")) {
+        if (enlargedCard === clickedCard) {
+          // If the clicked card is already enlarged, revert it to the original size
+          clickedCard.style.width = "180px";
+          clickedCard.style.height = "160px";
+          // clickedCard.style.transform = "scale(1)";
+          console.log("im clicked")
+          toggleFlip(clickedCard);
+          clickedCard.style.zIndex = "0";
+          enlargedCard = null;
+          // enlargedCard.style.zIndex = "0";
           postArea.classList.remove("enlarged");
-          clickedCard.style.transform = "scale(1)";
         } else {
+          // Revert the previously enlarged card (if any)
+          // if (enlargedCard) {
+          //   console.log("unclicked")
+          //   enlargedCard.style.zIndex = "0"; // Reset the z-index for the previously enlarged card
+          //   toggleFlip(enlargedCard);
+          // }
+
+          // Enlarge the clicked card
           postArea.classList.add("enlarged");
-          clickedCard.style.transform = "scale(1.2)";
-          bringCardToFront(clickedCard);
+          clickedCard.style.width = "222px"; // Adjust the width as needed
+          clickedCard.style.height = "160px"; // Adjust the height as needed
+          clickedCard.style.zIndex = "1"; // Set the z-index to bring it to the front
+          toggleFlip(clickedCard);
+          enlargedCard = clickedCard;
         }
-        toggleFlip(clickedCard);
       }
     });
   }
 });
+
+
 
 // Function to bring the clicked card to the front
 function bringCardToFront(card) {

949002bbf4b0f5dafe2331452d041868ef01263c
jacobhellenbrand <112660239+jacobhellenbrand@users.noreply.github.com>
2023-11-06 15:53:48 -0600

    added some css which kind of works

--- pages/testFolder/testScript.js
+++ pages/testFolder/testScript.js
@@ -64,6 +64,41 @@
 // });
 
 
+// document.addEventListener("DOMContentLoaded", function () {
+//   const postGrid = document.querySelector(".postGrid");
+//   const postArea = document.querySelector(".postArea");
+//   let enlargedCard = null;
+
+//   if (postGrid && postArea) {
+//     postGrid.addEventListener("click", function (e) {
+//       const clickedCard = e.target.closest(".flipdiv.v");
+//       if(!clickedCard) {
+//         enlargedCard.style.zIndex = "0";
+//       }
+//       if (clickedCard) {
+//         if (enlargedCard === clickedCard) {
+//           // If the clicked card is already enlarged, revert it to the original size
+//           clickedCard.style.width = "180px";
+//           clickedCard.style.height = "160px";
+//           console.log("im clicked")
+//           toggleFlip(clickedCard);
+//           clickedCard.style.zIndex = "0";
+//           enlargedCard = null;
+//           postArea.classList.remove("enlarged");
+//         } else {
+//           // Enlarge the clicked card
+//           postArea.classList.add("enlarged");
+//           clickedCard.style.width = "222px"; // Adjust the width as needed
+//           clickedCard.style.height = "160px"; // Adjust the height as needed
+//           clickedCard.style.zIndex = "1"; // Set the z-index to bring it to the front
+//           toggleFlip(clickedCard);
+//           enlargedCard = clickedCard;
+//         }
+//       }
+//     });
+//   }
+// });
+
 document.addEventListener("DOMContentLoaded", function () {
   const postGrid = document.querySelector(".postGrid");
   const postArea = document.querySelector(".postArea");
@@ -72,35 +107,31 @@ document.addEventListener("DOMContentLoaded", function () {
   if (postGrid && postArea) {
     postGrid.addEventListener("click", function (e) {
       const clickedCard = e.target.closest(".flipdiv.v");
-      if(!clickedCard) {
-        enlargedCard.style.zIndex = "0";
-      }
+
       if (clickedCard) {
         if (enlargedCard === clickedCard) {
           // If the clicked card is already enlarged, revert it to the original size
           clickedCard.style.width = "180px";
           clickedCard.style.height = "160px";
-          // clickedCard.style.transform = "scale(1)";
-          console.log("im clicked")
           toggleFlip(clickedCard);
-          clickedCard.style.zIndex = "0";
+          clickedCard.classList.remove("enlarged-card"); // Remove the custom class
           enlargedCard = null;
-          // enlargedCard.style.zIndex = "0";
           postArea.classList.remove("enlarged");
         } else {
           // Revert the previously enlarged card (if any)
-          // if (enlargedCard) {
-          //   console.log("unclicked")
-          //   enlargedCard.style.zIndex = "0"; // Reset the z-index for the previously enlarged card
-          //   toggleFlip(enlargedCard);
-          // }
+          if (enlargedCard) {
+            enlargedCard.style.width = "180px";
+            enlargedCard.style.height = "160px";
+            toggleFlip(enlargedCard);
+            enlargedCard.classList.remove("enlarged-card"); // Remove the custom class
+          }
 
           // Enlarge the clicked card
           postArea.classList.add("enlarged");
           clickedCard.style.width = "222px"; // Adjust the width as needed
           clickedCard.style.height = "160px"; // Adjust the height as needed
-          clickedCard.style.zIndex = "1"; // Set the z-index to bring it to the front
           toggleFlip(clickedCard);
+          clickedCard.classList.add("enlarged-card"); // Add the custom class
           enlargedCard = clickedCard;
         }
       }
@@ -108,6 +139,22 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 });
 
+
+
+
+
+// function getColumnIndexOfCard(card) {
+//   const cardLeft = card.style.left;
+//   if (cardLeft) {
+//     const leftValue = parseInt(cardLeft, 10);
+//     if (!isNaN(leftValue)) {
+//       const columnWidth = 320; // Adjust this value based on your actual column width
+//       const columnIndex = Math.floor(leftValue / columnWidth);
+//       return columnIndex;
+//     }
+//   }
+//   return 0; // Default to the first column if the left value is not found or invalid
+// }
 
 
 // Function to bring the clicked card to the front

4e0bc12b7f72c29415e4cfa21dc91dc266f87158
jacobhellenbrand <112660239+jacobhellenbrand@users.noreply.github.com>
2023-11-15 14:11:55 -0600

    updates to user account apge edit button

--- pages/accountPage/script.js
+++ pages/accountPage/script.js
@@ -10,9 +10,15 @@ function openModal() {
 
     document.getElementById('descrip').value = document.getElementById('title').innerText;
 
-    // Get the current profile image source from the card and set it as the selected option
-    var currentImageSrc = document.getElementById('cardImage').getAttribute('src');
-    document.getElementById('profilePhoto').value = currentImageSrc;
+    // // Get the current profile image source from the card and set it as the selected option
+    // var currentImageSrc = document.getElementById('cardImage').getAttribute('src');
+    // document.getElementById('profilePhoto').value = currentImageSrc;
+
+    // Get the current profile image source from the card and set it as the checked option
+  var currentImageSrc = document.getElementById('cardImage').getAttribute('src');
+  document.querySelector(`input[name="profilePhoto"][value="${currentImageSrc}"]`).checked = true;
+
+
 }
 
 function closeModal() {
@@ -25,7 +31,13 @@ document.getElementById('editForm').addEventListener('submit', function(event) {
     // Update card content with form data
     document.getElementById('cardName').innerText = document.getElementById('username').value;
     document.getElementById('title').innerText = document.getElementById('descrip').value;
-    document.getElementById('cardImage').setAttribute('src', document.getElementById('profilePhoto').value);
+    // document.getElementById('cardImage').setAttribute('src', document.getElementById('profilePhoto').value);
+
+        // Get the selected image value
+  var selectedImage = document.querySelector('input[name="profilePhoto"]:checked').value;
+
+  // Update the card image source
+  document.getElementById('cardImage').setAttribute('src', selectedImage);
 
     closeModal();
 });
--- pages/testFolder/test.js
+++ pages/testFolder/test.js
@@ -1,51 +1,13 @@
-//test for getting url value from attr
-// var img1 = $('.test').attr("data-thumbnail");
-// console.log(img1);
+    // Function to handle image selection
+    function selectImage(element) {
+      // Remove the 'selected' class from all image items
+      const imageItems = document.querySelectorAll('.image-item');
+      imageItems.forEach(item => item.classList.remove('selected'));
 
-//test for iterating over child elements
-var langArray = [];
-('.vodiapicker option').each(function(){
-  var img = $(this).attr("data-thumbnail");
-  var text = this.innerText;
-  var value = $(this).val();
-  var item = '<li><img src="'+ img +'" alt="" value="'+value+'"/><span>'+ text +'</span></li>';
-  langArray.push(item);
-})
+      // Add the 'selected' class to the clicked image item
+      element.classList.add('selected');
 
-('#a').html(langArray);
-
-//Set the button value to the first el of the array
-('.btn-select').html(langArray[0]);
-('.btn-select').attr('value', 'en');
-
-//change button stuff on click
-('#a li').click(function(){
-   var img = $(this).find('img').attr("src");
-   var value = $(this).find('img').attr('value');
-   var text = this.innerText;
-   var item = '<li><img src="'+ img +'" alt="" /><span>'+ text +'</span></li>';
-  ('.btn-select').html(item);
-  ('.btn-select').attr('value', value);
-  (".b").toggle();
-  //console.log(value);
-});
-
-(".btn-select").click(function(){
-        $(".b").toggle();
-    });
-
-//check local storage for the lang
-var sessionLang = localStorage.getItem('lang');
-if (sessionLang){
-  //find an item with value of sessionLang
-  var langIndex = langArray.indexOf(sessionLang);
-  ('.btn-select').html(langArray[langIndex]);
-  ('.btn-select').attr('value', sessionLang);
-} else {
-   var langIndex = langArray.indexOf('ch');
-  console.log(langIndex);
-  ('.btn-select').html(langArray[langIndex]);
-  //$('.btn-select').attr('value', 'en');
-}
-
-
+      // You can retrieve the selected image path using:
+      const selectedImagePath = element.querySelector('img').src;
+      console.log('Selected Image Path:', selectedImagePath);
+  }
